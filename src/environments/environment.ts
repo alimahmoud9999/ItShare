8f52a7ed417b30df238f8ec5884585518be2fded
unknown <alimahmoud9999>
2020-06-01 22:25:53 +0300

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyB-07mq1m1X43tfHX2GcbOtI0kBoEeORfc",
+    authDomain: "itshare-cf32f.firebaseapp.com",
+    databaseURL: "https://itshare-cf32f.firebaseio.com",
+    projectId: "itshare-cf32f",
+    storageBucket: "itshare-cf32f.appspot.com",
+    messagingSenderId: "268603387041",
+    appId: "1:268603387041:web:07c740a98af12ae79dd4e0",
+    measurementId: "G-W2DK40358X"
+  }
+
 };
 
 /*
